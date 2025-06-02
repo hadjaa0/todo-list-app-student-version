9c548ef5dc4cec4f9fc3da3a22b128bf0a8561bc
hadjaa0 <HAdjad10@nycstudents.net>
2025-06-02 14:11:32 -0400

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,15 +1,24 @@
 const db = require("../db");
 
-//Add a comment describing what this function does and what does it return
+/**
+ * Retrieves all tasks from the database, ordered by creation date descending.
+ * @returns {Promise<Array>} An array of task objects.
+ */
 const getTasks = async () => {
   const res = await db.query(
-    //correct this SQL query to select all tasks from the database
-    "SELECT everything FROM tasks ORDER BY created_at DESC"
+    // Corrected SQL query to select all columns from the tasks table
+    "SELECT * FROM tasks ORDER BY created_at DESC"
   );
   return res.rows;
-};
+}
 
-//Write a comment describing what this function insersts a new task into the database
+/**
+ * Inserts a new task into the database with the given title and description.
+ * Sets is_complete to false and created_at to the current timestamp.
+ * @param {string} title - The title of the task.
+ * @param {string} description - The description of the task.
+ * @returns {Promise<Object>} The newly created task object.
+ */
 const addTask = async (title, description) => {
   const res = await db.query(
     "INSERT INTO tasks (title, description, is_complete, created_at) VALUES ($1, $2, false, NOW()) RETURNING *",
